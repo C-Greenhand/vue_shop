5e379f0c5471671b5296d7d15ba8c5e657fcf6e3
C-Greenhand <3090622807@qq.com>
2020-05-09 09:39:40 +0800

    完成登录功能

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './plugins/element.js'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./plugins/element.js";
+import "./assets/css/global.css";
 
-Vue.config.productionTip = false
+import axios from "axios";
+//配置请求的路径
+axios.defaults.baseURL = "http://timemeetyou.com:8889/api/private/v1/";
+Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
