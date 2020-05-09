13a9a60ef44c0d24f7f874c39a41e16d02aebb2e
C-Greenhand <3090622807@qq.com>
2020-05-09 13:37:15 +0800

    侧边栏路由模式

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,12 +7,21 @@
       </div>
       <el-button type="info" @click="logout">退出</el-button>
     </el-header>
-    <!--页面主题区域-->
+    <!--页面主体区域-->
     <el-container>
       <!--侧边栏-->
-      <el-aside width="200px">
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!--侧边栏菜单区域-->
-        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          :unique-opened="true"
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          :router="true"
+        >
           <!--一级菜单-->
           <el-submenu :index="item.id+''" v-for="item in menulist" :key="item.id">
             <!--一级菜单模板区域-->
@@ -23,7 +32,11 @@
               <span>{{item.authName}}</span>
             </template>
             <!--二级菜单-->
-            <el-menu-item :index="subItem.id+''" v-for="subItem in item.children" :key="subItem.id">
+            <el-menu-item
+              :index="'/'+subItem.path+''"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+            >
               <template slot="title">
                 <!--图标-->
                 <i class="el-icon-menu"></i>
@@ -35,7 +48,10 @@
         </el-menu>
       </el-aside>
       <!--main区域-->
-      <el-main>Main</el-main>
+      <el-main>
+        <!--放置路由占位符-->
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
@@ -46,7 +62,8 @@
 export default {
   data() {
     return {
-      menulist: []
+      menulist: [],
+      isCollapse: false
     };
   },
   created() {
@@ -63,6 +80,10 @@ export default {
       if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
       this.menulist = res.data;
       console.log(res);
+    },
+    //折叠菜单栏与展开
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse;
     }
   }
 };
@@ -92,9 +113,22 @@ export default {
 
 .el-aside {
   background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
 }
 
 .el-main {
   background-color: #eaedf1;
+}
+
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
 }
 </style>
